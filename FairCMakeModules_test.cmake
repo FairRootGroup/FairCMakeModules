83768ea88a30cb84f1d29e737cf9bbbd5f1c8f0e
Christian Tacke <58549698+ChristianTackeGSI@users.noreply.github.com>
2021-04-24 12:59:49 +0200

    Add initial simple CTest suite
    * configures, builds, and installs the project
    * Use GitHub Actions to run it

---
+++ FairCMakeModules_test.cmake
@@ -0,0 +1,25 @@
+################################################################################
+#    Copyright (C) 2021 GSI Helmholtzzentrum fuer Schwerionenforschung GmbH    #
+#                                                                              #
+#              This software is distributed under the terms of the             #
+#              GNU Lesser General Public Licence (LGPL) version 3,             #
+#                  copied verbatim in the file "LICENSE"                       #
+################################################################################
+
+set(CTEST_SOURCE_DIRECTORY .)
+set(CTEST_BINARY_DIRECTORY build)
+set(CTEST_CMAKE_GENERATOR "Unix Makefiles")
+
+ctest_empty_binary_directory("${CTEST_BINARY_DIRECTORY}")
+ctest_start(Continuous)
+
+get_filename_component(test_install_prefix "${CTEST_BINARY_DIRECTORY}/install"
+                       ABSOLUTE)
+list(APPEND options
+  "-Wdev"
+  "-Werror=dev"
+  "-DCMAKE_INSTALL_PREFIX:PATH=${test_install_prefix}"
+)
+ctest_configure(OPTIONS "${options}")
+
+ctest_build(TARGET install)
