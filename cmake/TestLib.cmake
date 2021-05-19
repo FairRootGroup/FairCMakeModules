bf7b599c985a3e1ea421da69f5be3245086bd916
Dennis Klein <d.klein@gsi.de>
2021-05-19 22:06:09 +0200

    TestLib: Add PREFIX_PATH option to add_module_test

--- cmake/TestLib.cmake
+++ cmake/TestLib.cmake
@@ -65,10 +65,20 @@ endfunction()
 
 
 #
-# most useful to call within a CMakeLists.txt added via add_module_tests
+# add_module_tests(SUITE <suite> TEST <test>
+#                  [MODULE <module>]
+#                  [MODULE_PATH <module_path>]
+#                  [PREFIX_PATH <prefix_path>])
+#
+# Meant to be called within a CMakeLists.txt added via `add_module_tests`.
+#
+# `MODULE` defaults to the content of the CMake variable `MODULE` in the calling scope.
+# `MODULE_PATH` is prepended to the `CMAKE_MODULE_PATH`.
+# `PREFIX_PATH` is prepended to the `CMAKE_PREFIX_PATH`. If not given, it defaults to
+# `CMAKE_CURRENT_BINARY_DIR`.
 #
 function(add_module_test)
-  cmake_parse_arguments(PARSE_ARGV 0 ARGS "" "MODULE;SUITE;TEST" "MODULE_PATH")
+  cmake_parse_arguments(PARSE_ARGV 0 ARGS "" "MODULE;SUITE;TEST;PREFIX_PATH" "MODULE_PATH")
 
   if(ARGS_MODULE)
     set(__module__ ${ARGS_MODULE})
@@ -96,10 +106,17 @@ function(add_module_test)
     list(JOIN __module_path__ ";" __module_path__)
   endif()
 
+  if(NOT ARGS_PREFIX_PATH)
+    list(APPEND __prefix_path__ ${CMAKE_CURRENT_BINARY_DIR} ${CMAKE_PREFIX_PATH})
+  else()
+    list(APPEND __prefix_path__ ${ARGS_PREFIX_PATH} ${CMAKE_PREFIX_PATH})
+  endif()
+  list(JOIN __prefix_path__ ";" __prefix_path__)
+
   add_test(NAME ${__fqtn__} COMMAND ${CMAKE_COMMAND}
     -D "CMAKE_MODULE_PATH=${__module_path__}"
     -D "MODULE=${__module__}"
-    -D "CMAKE_PREFIX_PATH=${CMAKE_CURRENT_BINARY_DIR};${CMAKE_CURRENT_SOURCE_DIR}"
+    -D "CMAKE_PREFIX_PATH=${__prefix_path__}"
     -D "TEST=${CMAKE_CURRENT_SOURCE_DIR}/${ARGS_SUITE}_${ARGS_TEST}.cmake"
     -P "${CMAKE_SOURCE_DIR}/tests/test_module.cmake"
   )
