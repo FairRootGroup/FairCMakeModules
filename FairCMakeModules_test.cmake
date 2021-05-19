6666e2f5273bbbabfb2808e86f7d236221d70f0e
Dennis Klein <d.klein@gsi.de>
2021-05-20 00:18:25 +0200

    CI: Improve workflow
    * Test all supported CMake versions
    * Output CMake version
    * Test latest CMake version on macOS
    * Split build into multiple steps
    * Reduce log noise in test step

--- FairCMakeModules_test.cmake
+++ FairCMakeModules_test.cmake
@@ -6,26 +6,48 @@
 #                  copied verbatim in the file "LICENSE"                       #
 ################################################################################
 
+cmake_minimum_required(VERSION 3.15 FATAL_ERROR)
+cmake_policy(VERSION 3.15...3.20)
+
+if(NOT STEPS)
+  list(APPEND STEPS clean configure build install test)
+endif()
+
 set(CTEST_SOURCE_DIRECTORY .)
 set(CTEST_BINARY_DIRECTORY build)
 set(CTEST_CMAKE_GENERATOR "Unix Makefiles")
-
-ctest_empty_binary_directory("${CTEST_BINARY_DIRECTORY}")
-ctest_start(Continuous)
-
 get_filename_component(test_install_prefix "${CTEST_BINARY_DIRECTORY}/install"
                        ABSOLUTE)
-list(APPEND options
-  "-Wdev"
-  "-Werror=dev"
-  "-DCMAKE_INSTALL_PREFIX:PATH=${test_install_prefix}"
-)
-ctest_configure(OPTIONS "${options}")
 
-ctest_build(TARGET install)
-ctest_test(RETURN_VALUE _ctest_test_ret_val)
-
-if (_ctest_test_ret_val)
-  message(FATAL_ERROR " \n"
-          "   /!\\  Some tests failed.")
+if(clean IN_LIST STEPS)
+  file(REMOVE_RECURSE ${CTEST_BINARY_DIRECTORY})
+  file(REMOVE_RECURSE ${test_install_prefix})
+  ctest_start(Continuous)
+else()
+  ctest_start(Continuous APPEND QUIET)
 endif()
+
+if(configure IN_LIST STEPS)
+  list(APPEND options
+    "-Wdev"
+    "-Werror=dev"
+    "-DCMAKE_INSTALL_PREFIX:PATH=${test_install_prefix}"
+  )
+  ctest_configure(OPTIONS "${options}")
+endif()
+
+if(build IN_LIST STEPS)
+  ctest_build()
+endif()
+
+if(install IN_LIST STEPS)
+  ctest_build(TARGET install)
+endif()
+
+if(test IN_LIST STEPS)
+  ctest_test(RETURN_VALUE __ret)
+  if(__ret)
+    message(FATAL_ERROR "Some tests failed.")
+  endif()
+endif()
+
