8627efe499310daa130747b0fdd10a147819fec2
Christian Tacke <58549698+ChristianTackeGSI@users.noreply.github.com>
2021-05-24 20:33:08 +0200

    feature(FairFindPackage2): Handle OPTIONAL_COMPONENTS
    find_package2 used to handle OPTIONAL_COMPONENTS by just
    passing it down to find_package().
    if find_package2 interprets the optional components as part
    of the arguments to COMPONENTS, the word OPTIONAL_COMPONENTS
    will be part of PROJECT_INTERFACE_${pkgname}_COMPONENTS.
    
    So parse OPTIONAL_COMPONENTS in find_package2 and export
    PROJECT_INTERFACE_${pkgname}_OPTIONAL_COMPONENTS.
    
    fair_generate_package_dependencies needs to export this
    now.
    
    And find_package2 obeys the imported settings now.
    
    Dennis Klein gets most of the credit for these changes.

--- tests/FairFindPackage2/fair_generate_package_dependencies_simple.cmake
+++ tests/FairFindPackage2/fair_generate_package_dependencies_simple.cmake
@@ -9,7 +9,8 @@
 set(PROJECT_NAME fair_generate_package_dependencies_simple)
 
 find_package2(PRIVATE FooBar)
-find_package2(PUBLIC FooDep VERSION 2.5)
+find_package2(PUBLIC FooDep VERSION 2.5
+              COMPONENTS comp1 OPTIONAL_COMPONENTS comp3)
 
 fair_generate_package_dependencies()
 cmake_print_variables(PACKAGE_DEPENDENCIES)
@@ -18,4 +19,6 @@ if(COMMAND cmake_language)
   cmake_language(EVAL CODE "${PACKAGE_DEPENDENCIES}")
   assert_var_equal(${PROJECT_NAME}_PACKAGE_DEPENDENCIES "FooDep")
   assert_var_equal(${PROJECT_NAME}_FooDep_VERSION "2.5")
+  assert_var_equal(${PROJECT_NAME}_FooDep_COMPONENTS "comp1")
+  assert_var_equal(${PROJECT_NAME}_FooDep_OPTIONAL_COMPONENTS "comp3")
 endif()
